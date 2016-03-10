8150d2dd3e669591a5cc5d8d1b4f27d39b7e44bb
Mia Gaulin <gaulin.mia@gmail.com>
2016-03-10 13:43:24 -0500

    Initial

---
+++ server.rb
@@ -0,0 +1,23 @@
+require "sinatra"
+require "pg"
+
+configure :development do
+  set :db_config, { dbname: "movies" }
+end
+
+configure :test do
+  set :db_config, { dbname: "movies_test" }
+end
+
+def db_connection
+  begin
+    connection = PG.connect(Sinatra::Application.db_config)
+    yield(connection)
+  ensure
+    connection.close
+  end
+end
+
+get '/actors' do
+
+end
